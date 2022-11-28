6d00f7223f1a79eacf755f55a0ace61238073ebc
OksMor <Sancho_2006_ru@mail.ru>
2022-11-28 15:09:17 +0300

    аутентификация

--- project/src/pages/sing-in-screen/sing-in-screen.tsx
+++ project/src/pages/sing-in-screen/sing-in-screen.tsx
@@ -1,9 +1,47 @@
+import { useRef, FormEvent, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { Helmet } from 'react-helmet-async';
+import { AuthData } from '../../types/types';
+import { loginAction } from '../../store/api-actions';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 import Logo from '../../components/logo/logo';
 import Copyright from '../../components/copyright/copyright';
 
 function SingInScreen(): JSX.Element {
+
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  const valid = '[A-Za-z]+[0-9]|[0-9]+[A-Za-z]';
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        login: loginRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (authorizationStatus === AuthorizationStatus.Auth) {
+      navigate(AppRoute.Root);
+    }
+  }, [authorizationStatus, navigate]);
+
   return (
     <div className="user-page">
       <Helmet>
@@ -18,19 +56,19 @@ function SingInScreen(): JSX.Element {
       </header>
 
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form action="#" className="sign-in__form" onSubmit={handleSubmit}>
           <div className="sign-in__fields">
             <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" ref={loginRef} required />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" pattern={valid} ref={passwordRef} required />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
           <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">Sign in</button>
+            <button className="sign-in__btn" type="submit" >Sign in</button>
           </div>
         </form>
       </div>
--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,20 +1,10 @@
 import { AxiosInstance } from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AppDispatch, State } from '../types/state';
-import { Film } from '../types/types';
-import { APIRoute, TIMEOUT_SHOW_ERROR, AuthorizationStatus } from '../const';
-import { loadFilms, setFilmsLoadingStatus, setError, requireAuthorization } from './action';
-import {store} from './';
-
-export const clearErrorAction = createAsyncThunk(
-  'data/clearError',
-  () => {
-    setTimeout(
-      () => store.dispatch(setError(null)),
-      TIMEOUT_SHOW_ERROR,
-    );
-  },
-);
+import { Film, AuthData, UserData } from '../types/types';
+import { APIRoute, AuthorizationStatus, AppRoute } from '../const';
+import { loadFilms, setFilmsLoadingStatus, requireAuthorization, redirectToRoute } from './action';
+import { saveToken, dropToken } from '../services/token';
 
 export const fetchFilms = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -45,3 +35,42 @@ export const checkAuthAction = createAsyncThunk<void, undefined, {
     }
   },
 );
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/login',
+  async ({login: email, password}, {dispatch, extra: api}) => {
+    const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
+    saveToken(token);
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    dispatch(redirectToRoute(AppRoute.Root));
+  },
+);
+
+export const logoutAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/logout',
+  async (_arg, {dispatch, extra: api}) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+  },
+);
+
+// export const fetchPromoFilm = createAsyncThunk<Film, undefined, {
+//   dispatch: AppDispatch;
+//   state: State;
+//   extra: AxiosInstance;
+// }>(
+//   'data/promoFilm',
+//   async (_arg, { extra: api }) => {
+//     const { data } = await api.get<Film>(APIRoute.Promo);
+//     dispatch(loadFilms(data));
+//   },
+// );
